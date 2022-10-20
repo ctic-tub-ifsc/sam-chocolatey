f81ae63c91979f5cde217e7e09b2b2991e73566b
Tiago Ferreira <tiagojulianoferreira@gmail.com>
2022-10-20 19:07:20 +0000

    SAM Chocolatey Package funtional

---
+++ tools/chocolateybeforemodify.ps1
@@ -0,0 +1,6 @@
+﻿$packageName = 'sam'
+$fileType = 'exe'
+$url = 'https://sam.nrel.gov/download/66-sam-2021-12-02-for-windows/file.html'
+$silentArgs = '/S'
+
+Install-ChocolateyPackage $packageName $fileType $silentArgs $url
